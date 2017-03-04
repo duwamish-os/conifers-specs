244f7e00421aa0d8b65dd41899d95eb7e6a1c087
prayagupd <prayag.upd@gmail.com>
2017-03-04 03:49:42 -0800

    add http flow specs

---
+++ src/main/scala/com/specs/http/HttpFlowSpecs.scala
@@ -0,0 +1,42 @@
+package com.specs.http
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.stream.Collectors
+
+import com.specs.FlowSpecs
+import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet, HttpPost}
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.DefaultHttpClient
+
+import scala.util.parsing.json.{JSON, JSONType}
+
+/**
+  * Created by prayagupd
+  * on 3/4/17.
+  */
+
+class HttpFlowSpecs extends FlowSpecs {
+
+  def doHttpGet(endpoint: String): CloseableHttpResponse = {
+    val httpRequest = new HttpGet(endpoint)
+    val response = (new DefaultHttpClient).execute(httpRequest)
+    response
+  }
+
+  def doHttpPost(endpoint: String, content: String, contentType: String = "application/json"): CloseableHttpResponse = {
+    val httpRequest = new HttpPost(endpoint)
+    httpRequest.setHeader("Content-type", contentType)
+    httpRequest.setEntity(new StringEntity(content))
+
+    val response = (new DefaultHttpClient).execute(httpRequest)
+    response
+  }
+
+  def responseContent(response: CloseableHttpResponse): String = {
+    val responseBody: String = new BufferedReader(new InputStreamReader(response.getEntity.getContent))
+      .lines().collect(Collectors.joining("\n"))
+
+    println("Response body = " + responseBody)
+    responseBody
+  }
+}
---
+++ src/test/scala/com/specs/flow/http/SomeHttpFlowSpecs.scala
@@ -0,0 +1,37 @@
+package com.specs.flow.http
+
+import com.specs.http.HttpFlowSpecs
+
+import scala.util.parsing.json._
+
+/**
+  * Created by prayagupd
+  * on 2/27/17.
+  */
+
+class SomeHttpFlowSpecs extends HttpFlowSpecs {
+
+  feature("Getting user posts on my API server") {
+
+    scenario("As a software engineer, I want to receive the user posts when I call the endpoint") {
+
+      When("I send a GET request to the http endpoint")
+      val response = doHttpGet("https://jsonplaceholder.typicode.com/posts/1")
+
+      Then("I receive 200 response back with the user posts")
+      assert(response.getStatusLine.getStatusCode == 200)
+
+      val expectedJson =
+        """
+          |{
+          |  "userId": 1,
+          |  "id": 1,
+          |  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          |  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+          |}
+        """.stripMargin
+
+      assert(JSON.parseRaw(responseContent(response)) == JSON.parseRaw(expectedJson))
+    }
+  }
+}
