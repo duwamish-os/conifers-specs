f5b8869f51a42baafd0b4ec09a7fd4d41649ae31
prayagupd <prayag.upd@gmail.com>
2017-06-10 01:13:26 -0700

    pass CI env parameter as maven argLine (eg. -DCI_APPLICATION_ENVIRONMENT=e2e)

--- src/main/scala/com/specs/config/ConfigLoader.scala
+++ src/main/scala/com/specs/config/ConfigLoader.scala
@@ -2,19 +2,33 @@ package com.specs.config
 
 import java.util.Properties
 
+import sys.process._
+import scala.language.postfixOps
+
 /**
   * Created by prayagupd
   * on 4/4/17.
   */
 class ConfigLoader {
+
   var appConfig: Properties = new Properties() {
     load(ConfigLoader.this.getClass.getClassLoader.getResourceAsStream(config))
   }
 
   def environment: String = {
-    val applicationEnvironment: String = System.getenv("APPLICATION_ENVIRONMENT")
-    println(s"application environment is set to $applicationEnvironment")
-    applicationEnvironment
+
+    Option(System.getProperty("CI_APPLICATION_ENVIRONMENT")) match {
+      case Some(env) =>
+        println("-DCI_APPLICATION_ENVIRONMENT")
+        println(s"application CI environment is set to $env")
+        env
+      case None =>
+        println("============ infra environment ============")
+        println("env"!!)
+        println("============ infra environment ============")
+        println(s"application environment is set to ${System.getenv("APPLICATION_ENVIRONMENT")}")
+        System.getenv("APPLICATION_ENVIRONMENT")
+    }
   }
 
   def config: String = {
@@ -25,4 +39,3 @@ class ConfigLoader {
   private def configExists(env: String): Boolean =
     classOf[ConfigLoader].getClassLoader.getResource("application-" + env + ".properties") != null
 }
-
